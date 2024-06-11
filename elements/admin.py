e64ef6c5333dc9bbfd42326bb4820127e43bafe0
Luis Sucuc <jlsucuc@gmail.com>
2024-06-11 12:35:45 -0600

    chore: Create Brand and Color models and add to the admin

--- elements/admin.py
+++ elements/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Person, Vehicle, Officer
+from .models import Person, Vehicle, Officer, Brand, Color
 
 
 @admin.register(Person)
@@ -12,14 +12,35 @@ class PersonAdmin(admin.ModelAdmin):
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ('plate', 'brand', 'color', 'person')
-    search_fields = ('plate', 'brand', 'color', 'person__name')
-    list_filter = ('brand', 'color')
+    search_fields = ('plate', 'brand', 'color', 'person')
+    list_filter = ('brand', 'color', 'person')
+
+    fieldsets = (
+        ('General Information', {
+            'fields': ('plate', 'brand', 'color')
+        }),
+        ('Owner Information', {
+            'fields': ('person',)
+        }),
+    )
 
 
 @admin.register(Officer)
 class OfficerAdmin(admin.ModelAdmin):
     list_display = ('name', 'id_number')
     search_fields = ('name', 'id_number')
+    list_filter = ('name',)
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
     list_filter = ('name',)
 
 
--- elements/models.py
+++ elements/models.py
@@ -2,27 +2,73 @@ from django.db import models
 
 
 class Person(models.Model):
+    """
+    Model that represents a person.
+    """
+
     name = models.CharField(max_length=255)
+    """name: Name of the person"""
     email = models.EmailField(unique=True)
+    """email: Unique email address of the person"""
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    """
+    Model that represents a brand.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+    """name: Unique name of the brand"""
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    """
+    Model that represents a color.
+    """
+
+    name = models.CharField(max_length=50, unique=True)
+    """name: Unique name of the color"""
 
     def __str__(self):
         return self.name
 
 
 class Vehicle(models.Model):
+    """
+    Model that represents a vehicle.
+    """
+
     plate = models.CharField(max_length=50, unique=True)
-    brand = models.CharField(max_length=100)
-    color = models.CharField(max_length=50)
+    """plate: Unique plate number of the vehicle"""
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name='brand')
+    """brand: Brand of the vehicle"""
+    color = models.ForeignKey(
+        Color, on_delete=models.CASCADE, related_name='color')
+    """color: Color of the vehicle"""
     person = models.ForeignKey(
-        Person, on_delete=models.CASCADE, related_name='vehicles')
+        Person, on_delete=models.CASCADE, related_name='owner')
+    """person: Owner of the vehicle"""
 
     def __str__(self):
         return f"{self.plate} - {self.brand}"
 
 
 class Officer(models.Model):
+    """
+    Model that represents an officer.
+    """
+
     name = models.CharField(max_length=255)
+    """name: Name of the officer"""
     id_number = models.CharField(max_length=100, unique=True)
+    """id_number: Unique identification number of the officer"""
 
     def __str__(self):
         return self.name

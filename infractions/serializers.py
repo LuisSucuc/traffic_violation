db6e0b482e2f51eff75d1fe0d28fd67246d086fb
Luis Sucuc <jlsucuc@gmail.com>
2024-06-11 23:14:59 -0600

    feat: Create endpoint for report of infractions

--- infractions/serializers.py
+++ infractions/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
 from .models import Infraction
+from elements.models import Vehicle
+from rest_framework import serializers
 
 
 class InfractionSerializer(serializers.ModelSerializer):
@@ -11,3 +12,17 @@ class InfractionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Infraction
         fields = ['id', 'vehicle', 'vehicle_plate', 'timestamp', 'comments']
+
+
+class InfractionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Infraction
+        fields = ['timestamp', 'comments']
+
+
+class VehicleInfractionReportSerializer(serializers.ModelSerializer):
+    infractions = InfractionSerializer(many=True)
+
+    class Meta:
+        model = Vehicle
+        fields = ['plate', 'infractions']
--- infractions/views.py
+++ infractions/views.py
@@ -4,6 +4,9 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .models import Infraction, Vehicle
 from .serializers import InfractionSerializer
+from .serializers import VehicleInfractionReportSerializer
+from django.core.validators import EmailValidator
+from django.core.exceptions import ValidationError
 
 
 class LoadInfractionView(APIView):
@@ -44,3 +47,40 @@ class LoadInfractionView(APIView):
         except Exception as e:
             return Response({"error": f"Internal server error: {str(e)}"},
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class GenerateReportView(APIView):
+    """
+    View to generate a report of the infractions of a person.
+    """
+    def get(self, request, *args, **kwargs):
+        email = request.query_params.get('email')
+        # Check if the email is provided
+        if not email:
+            return Response({"error": "Email is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Validate the email
+        try:
+            validator = EmailValidator()
+            validator(email)
+        except ValidationError:
+            return Response({"error": "Invalid email"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Get the vehicles of the person
+        vehicles = Vehicle.objects.filter(
+            person__email=email).prefetch_related('infraction_set')
+        
+        # Create the report data
+        report_data = [
+            {
+                'plate': vehicle.plate,
+                'infractions': vehicle.infraction_set.all()
+            }
+            for vehicle in vehicles
+        ]
+
+        serializer = VehicleInfractionReportSerializer(report_data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+            
+
+            
